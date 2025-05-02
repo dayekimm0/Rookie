c70bb62f7ec0286dfbbbfc67a834ce9aa3493f8d
yj901 <901duwls@gmail.com>
2025-05-02 18:29:11 +0900

    yeojin home #1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  border: 1px solid #f00;
+  width: 100%;
+  height: 180px;
+  background: var(--main);
+`;
+
 const Header = () => {
-  return <div>Header</div>;
+  return <Container>Header</Container>;
 };
 
 export default Header;
