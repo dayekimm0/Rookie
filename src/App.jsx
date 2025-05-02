67c0f74da9ef81648ec708102eed83548e8ca71b
Myeonkwan72 <audrhks072@gmail.com>
2025-05-02 16:48:58 +0900

    cart merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import GlobalStyles from "./styles/Globalstyles.styles";
 import Cart from "./pages/Cart";
-=======
-import GlobalStyles from "./styles/Globalstyles.styles";
 import Home from "./pages/Home";
->>>>>>> 1f5d7e381839ba51ecb943db839eeebfb7d2e640
 
 function App() {
   return (
     <>
       <GlobalStyles />
-<<<<<<< HEAD
       <Cart />
-=======
       <Home />
->>>>>>> 1f5d7e381839ba51ecb943db839eeebfb7d2e640
     </>
   );
 }
