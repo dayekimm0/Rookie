33260d5113db88790dd99af80f1076cd10afc6a9
a01040579861 <a01040579861@gmail.com>
2025-05-12 18:01:49 +0900

    seong-il Event page#3

--- src/components/Event/ButtonGroup.jsx
+++ src/components/Event/ButtonGroup.jsx
@@ -2,24 +2,64 @@ import React from "react";
 import styled from "styled-components";
 
 const Buttons = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 0fr);
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 70px;
   color: var(--gray1);
+  padding-bottom: 13%;
+  @media screen and (max-width: 1024px) {
+  }
+  @media screen and (max-width: 500px) {
+    padding-bottom: 26%;
+  }
+  @media screen and (max-width: 375px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 const Button = styled.button`
-  font-size: 2.4rem;
-  padding: 20.5px 80px;
+  width: 300px;
+  height: 70px;
+  font-size: 2rem;
   border-radius: 50px;
   border: none;
   cursor: pointer;
   background: var(--main);
   font-weight: bold;
-  margin-right: 30px;
+  margin: 0px 20px 0px 20px;
   box-shadow: 0 12px 4px rgba(0, 0, 0, 0.1);
   transition: all 0.5s;
   &:hover {
     background: var(--light);
+  }
+  @media screen and (max-width: 1024px) {
+    width: 220px;
+    height: 55px;
+    font-size: 1.6rem;
+    box-shadow: 0 12px 4px rgba(0, 0, 0, 0.1);
+    transition: all 0.5s;
+    &:hover {
+      background: var(--light);
+    }
+  }
+  @media screen and (max-width: 500px) {
+    width: 180px;
+    height: 45px;
+    font-size: 1.2rem;
+    box-shadow: 0 12px 4px rgba(0, 0, 0, 0.1);
+    transition: all 0.5s;
+    &:hover {
+      background: var(--light);
+    }
+  }
+  @media screen and (max-width: 375px) {
+    width: 140px;
+    height: 44px;
   }
 `;
 
