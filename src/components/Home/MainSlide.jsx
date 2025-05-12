2f3adaeecf98c49441e2093181f8ad1d43f3eed4
daye <dayekimm0@gmail.com>
2025-05-12 17:20:32 +0900

    Revert "Productdetail [윤섭] (확대기능 추가)"

--- src/components/Home/MainSlide.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainSlide = () => {
-  return <div>MainSlide</div>;
-};
-
-export default MainSlide;
--- src/theme.js
+++
@@ -1,5 +0,0 @@
-const theme = {
-  red: "#e22531",
-};
-
-export default theme;
