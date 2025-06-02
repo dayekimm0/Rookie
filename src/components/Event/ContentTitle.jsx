4a3e4d0b73169d2d7141ffbbe030c3c4a79cc3bc
yj901 <901duwls@gmail.com>
2025-06-02 17:25:10 +0900

    yeojin event-font #1

--- src/components/Event/ContentTitle.jsx
+++ src/components/Event/ContentTitle.jsx
@@ -13,7 +13,7 @@ const Container = styled.div`
 `;
 
 const ContentOne = styled.div`
-  font-family: "Gmarketsans";
+  font-family: "GmarketSans";
   font-weight: bold;
   padding: 30px;
   user-select: none;
@@ -59,9 +59,11 @@ const MediumText = styled.div`
     color: var(--main);
   }
 `;
+
 const LastText = styled.div`
   font-size: 1.8rem;
-  font-family: "Pretendard";
+  font-family: "Pretendard", sans-serif;
+  font-weight: 400;
   @media screen and (max-width: 1024px) {
     font-size: 1.6rem;
     margin-bottom: 5%;
@@ -81,7 +83,6 @@ const LastText = styled.div`
 
 const ContentTwo = styled.div`
   margin-bottom: 50px;
-  font-family: "Pretendard";
   user-select: none;
   @media screen and (max-width: 1024px) {
     margin-bottom: 3%;
