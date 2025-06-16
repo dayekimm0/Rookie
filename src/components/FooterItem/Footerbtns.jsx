34f1eef3e0a62f4bdd98457fc8a0eb26fb35ea2a
yj901 <901duwls@gmail.com>
2025-06-16 21:16:54 +0900

    yeojin teamhomeBtn #5

---
+++ src/components/FooterItem/Footerbtns.jsx
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+import TeamhomeBtn from "./TeamhomeBtn";
+import { useLocation } from "react-router-dom";
+
+const UpbtnWrap = styled.div`
+  position: fixed;
+  z-index: 88;
+  right: 25px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  bottom: 25px;
+
+  @media screen and (max-width: 1024px) {
+    right: 3%;
+  }
+
+  @media screen and (max-width: 500px) {
+    right: 15px;
+    bottom: 15px;
+  }
+`;
+
+const Upbtn = styled.div`
+  width: 48px;
+  aspect-ratio: 1;
+  border-radius: 4px;
+  border: 1px solid;
+  border-color: ${({ $mode }) => ($mode === "light" ? "#aaa" : "#fff")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  backdrop-filter: blur(2px);
+  opacity: ${({ $isVisible }) => ($isVisible ? 1 : 0)};
+  visibility: ${({ $isVisible }) => ($isVisible ? "visible" : "hidden")};
+  transition: opacity 0.3s;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.1);
+
+  svg {
+    width: 22px;
+    path {
+      stroke: ${({ $mode }) => ($mode === "light" ? "#888" : "#fff")};
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    width: 44px;
+    svg {
+      width: 18px;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    width: 37px;
+    svg {
+      width: 16px;
+    }
+  }
+`;
+
+const Footerbtns = ({ mode, isVisible }) => {
+  const { pathname } = useLocation();
+
+  const isTeamhome = pathname.startsWith("/teamhome");
+
+  const scrollToTop = () => {
+    lenis.scrollTo(0); // Lenis scrollTop
+  };
+
+  return (
+    <UpbtnWrap>
+      {isTeamhome && <TeamhomeBtn mode={mode} isVisible={isVisible} />}
+      <Upbtn
+        onClick={scrollToTop}
+        $mode={mode}
+        $isVisible={isVisible}
+        data-lenis-prevent
+      >
+        <svg width="24" height="26" viewBox="0 0 24 26" fill="none">
+          <path
+            d="M22.8333 12.4206L12 1.58728L1.16667 12.4206"
+            stroke="#666666"
+            strokeWidth="1.66667"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M12 1.95544V24.4126"
+            stroke="#666666"
+            strokeWidth="1.66667"
+            strokeLinecap="round"
+          />
+        </svg>
+      </Upbtn>
+    </UpbtnWrap>
+  );
+};
+
+export default Footerbtns;
